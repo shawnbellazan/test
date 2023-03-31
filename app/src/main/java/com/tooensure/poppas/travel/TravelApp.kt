af5177677c49878292065e001e3968962455c430
tooensure.com <shawn@tooensure.com>
2023-03-31 13:12:54 -0500

    integrating dagger-hilt

---
+++ app/src/main/java/com/tooensure/poppas/travel/TravelApp.kt
@@ -0,0 +1,8 @@
+package com.tooensure.poppas.travel
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TravelApp: Application() {
+}
---
+++ app/src/main/java/com/tooensure/poppas/travel/di/AppModule.kt
@@ -0,0 +1,12 @@
+package com.tooensure.poppas.travel.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+}
+// integrating
